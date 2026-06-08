32e5c86664b898caf1d0e73d697dda48c9e21207
Chris Matrakidis <cmatraki@gmail.com>
2026-06-08 21:28:49 +0300

    fix option parsing without NDEBUG

--- src/mpl/mpl3.c
+++ src/mpl/mpl3.c
@@ -6865,6 +6865,8 @@ void execute_statement(MPL *mpl, STATEMENT *stmt)
          case A_IF: /* if then else */
             execute_if(mpl, stmt->u.if_stmt);
             break;
+         case A_OPTION: /* option is ignored for now */
+            break;
          default:
             xassert(stmt != stmt);
       }
